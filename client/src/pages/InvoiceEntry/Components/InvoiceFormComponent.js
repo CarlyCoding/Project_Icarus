d7bb652580387d8b6e7c2e7dcecf651708509492
CarlyCoding <carly_off@outlook.com>
2022-09-28 10:53:00 +0100

    Adding invoice entry screen code

--- client/src/pages/InvoiceEntry/Components/InvoiceFormComponent.js
+++ client/src/pages/InvoiceEntry/Components/InvoiceFormComponent.js
@@ -5,7 +5,29 @@ function InvoiceFormComponent(){
     const handlePoChange= (event) => {
         setPoNumber(event.target.value);
     }
-    // Do a setter for state for every field
+        // Supplier field should be dropdown from db- enter supplier here. 
+
+    const [invoice_number, setInvoiceNumber]= useState();
+    const handleInvoiceNumberEntry= (event) =>{
+        setInvoiceNumber(event.target.value);
+    }
+
+    const [invoice_date, setInvoiceDate]= useState();
+    const handleInvoiceDateEntry= (event) =>{
+        setInvoiceDate(event.target.value);
+    }
+
+    const [goods_description, setGoodsDescription]= useState();
+    const handleGoodsDescriptionEntry= (event) =>{
+        setGoodsDescription(event.target.value);
+    }
+
+
+
+
+
+
+    
     const handleSubmit= (event) => {
         event.preventDefault();
         alert("Clicked save");
@@ -15,6 +37,18 @@ function InvoiceFormComponent(){
             <label>
                 Po Number:
                 <input type="text" value={po_number} onChange={handlePoChange}/>
+            </label>
+            <label>
+                Invoice number:
+                <input type="text" value={invoice_number} onChange={handleInvoiceNumberEntry}/>
+            </label>
+            <label>
+                Invoice Date:
+                <input type="text" value={invoice_date} onChange={handleInvoiceDateEntry}/>
+            </label>
+            <label>
+                Description of goods:
+                <input type="text" value={goods_description} onChange={handleGoodsDescriptionEntry}/>
             </label>
             {/* Next labels should be the next fields */}
             <input type="submit" value="Submit"/>
